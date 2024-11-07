a95e059b2015825d5b486d0bdcfb38a6698aea65
Игорь Рябухин <igorr1970@mail.ru>
2024-11-07 23:40:04 +0500

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,12 +1,9 @@
 def add_everything_up(a, b):
     try:
-        # Попытка сложить a и b
         return a + b
     except TypeError:
-        # Если возникает ошибка типа, возвращаем строковое представление
         return str(a) + str(b)
 
-# Примеры использования функции
-print(add_everything_up(123.456, 'строка'))  # Вывод: 123.456строка
-print(add_everything_up('яблоко', 4215))      # Вывод: яблоко4215
-print(add_everything_up(123.456, 7))           # Вывод: 130.456
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
