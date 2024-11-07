4c594f6e0bb45cdbd1f9dac8471a52a4106c79fe
Игорь Рябухин <igorr1970@mail.ru>
2024-11-08 00:05:02 +0500

    Update module_8_2.py

--- module_8_2.py
+++ module_8_2.py
@@ -11,7 +11,6 @@ def personal_sum(numbers):
 
     return result, incorrect_data
 
-
 def calculate_average(numbers):
     try:
         total_sum, incorrect_data = personal_sum(numbers)
@@ -25,10 +24,8 @@ def calculate_average(numbers):
         print('В numbers записан некорректный тип данных')
         return None
 
-
-# Примеры вызова функции calculate_average
-print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
-print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
-print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
-print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
 
